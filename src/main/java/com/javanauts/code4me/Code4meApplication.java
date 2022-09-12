d4dd3cdf53d7c862f78e6d74a8e762cc45181f16
Mandy Mason <mnmason86@gmail.com>
2022-09-12 14:05:02 -0400

    initialize Spring app

---
+++ src/main/java/com/javanauts/code4me/Code4meApplication.java
@@ -0,0 +1,13 @@
+package com.javanauts.code4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code4meApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code4meApplication.class, args);
+	}
+
+}
