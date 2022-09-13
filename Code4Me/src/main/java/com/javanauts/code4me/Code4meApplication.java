30160e557cc8239cc7cf1b2278857714b1936399
DariusO92 <99936580+DariusO92@users.noreply.github.com>
2022-09-12 18:06:31 -0700

    Add files via upload
    added the packages for the repos, models and configs

---
+++ Code4Me/src/main/java/com/javanauts/code4me/Code4meApplication.java
@@ -0,0 +1,21 @@
+package com.javanauts.code4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Code4meApplication {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Code4meApplication.class, args);
+	}
+
+}
