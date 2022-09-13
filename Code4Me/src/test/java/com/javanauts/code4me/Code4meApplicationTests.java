d033659481e5548be66b1c13b9db9405900cb3e4
DariusO92 <99936580+DariusO92@users.noreply.github.com>
2022-09-12 17:54:25 -0700

    Add files via upload
    Added packages for the repo model and configs

---
+++ Code4Me/src/test/java/com/javanauts/code4me/Code4meApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauts.code4me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code4meApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
